b2bfe468e2eae4e4bef61aea10a604699aafbe7d
TiagoMabango <tiagomabango@gmail.com>
2022-09-13 13:59:12 +0100

    MVC

--- ControleDeContatos/Program.cs
+++ ControleDeContatos/Program.cs
@@ -1,4 +1,5 @@
 using ControleDeContatos.Data;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,11 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void Configuration(IConfiguration configuration)
+{
+    IConfiguration Configuration = configuration;
+}
 
 void ConfigureServices(IServiceCollection services)
 {
